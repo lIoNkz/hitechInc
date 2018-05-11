e56b9629ae53274f0afae0365ae4e9f5ff051710
Khodzha Abulkhair <khodzha_au@mai.ru>
2018-05-11 16:16:16 +0600

    fix

---
+++ resources/views/frontend/layouts/master.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru" class="wf-lato-n4-active wf-lato-n7-active wf-active">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" content="{{ csrf_token() }}" />
+	<meta name="description" content="">
+	<meta name="theme-color" content="#0c9737">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="создание сайтов, разработка сайтов, цена сайта, Шымкент, Казахстан, Астана, Алматы, заказ сайта, заказать сайт, сайт визитка, бизнес сайт, веб студия, hitech">
+  	<meta property="og:type" content="website">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="Веб-студия Hitech Inc">
+    <meta property="og:locale" content="ru_RU">
+    <meta property="og:url" content="https://qaz-tech.kz/">
+    <meta property="og:site_name" content="Hitech Inc">
+
+
+	<title>Создание и разработка сайтов в HITECH Inc – Шымкент</title>
+<!-- 	<link rel="shortcut icon" href="https://megagroup.ru/favicon.ico" type="image/x-icon"> -->
+
+
+  	<link rel="stylesheet" href="/css/slick.min.css">
+  	<link rel="stylesheet" href="/css/all.min.css">
+  	<link rel="stylesheet" href="/css/style.css">
+  	<link rel="stylesheet" href="/css/partials.css">
+	<link rel="stylesheet" href="/css/app.css">
+
+</head>
+<body class="cd-notouch">
+	
+		@include('frontend.partials._svgBlock')
+	<header>
+		@include('frontend.partials._header')
+	</header>
+	<main>
+		@yield('content')
+	</main>
+	<footer class="mp-footer">
+		@include('frontend.partials._footer')
+	</footer>
+
+
+	<script  src="/js/jquery-3.3.1.min.js"></script>
+	<script  src="/js/slick.min.js"></script>
+	<script  src="/js/app.js"></script>
+</body>
+</html>
---
+++ resources/views/frontend/partials/_footer.blade.php
@@ -0,0 +1,164 @@
+<footer>
+  <div class="mp-wrapp mp-footer__wrapp">
+    <div class="mp-footer__info">
+      <a href="https://megagroup.ru/" alt="megagroup.ru" class="mp-img mp-footer__logo">
+        <img src="./Создание и разработка сайтов в Мегагрупп.ру – Москва, Санкт-Петербург и вся Россия_files/pic-footer-logo.svg" alt="megagroup.ru">
+      </a>
+      <div class="mp-footer__text">
+        <p>Мы занимается разработкой сайтов для России и зарубежных рынков с 1997 года. Наша команда – более пятисот
+          сотрудников в разных странах и городах.</p>
+        <p>Понимание потребностей онлайн-бизнеса, опыт взаимодействия с тысячами клиентов, высокая квалификация
+          персонала, собственные «ноу-хау» и хороший объём заказов позволяют Мегагрупп.ру предлагать услуги высокого
+          качества по удивительно низким и «вкусным» ценам.</p>
+      </div>
+      <p class="mp-footer__copy">© 1997–2018</p>
+      <a href="https://megagroup.ru/privacy" class="mp-footer__privacy-link">Политика конфиденциальности</a>
+    </div>
+    <div class="mp-footer__nav">
+      <div class="mp-footer__column">
+        <div class="mp-footer__group">
+          <p class="mp-footer__group-title">Компания</p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/compani" class="mp-footer__group-link">О Megagroup.ru</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/mission" class="mp-footer__group-link">Наша миссия</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/team" class="mp-footer__group-link">Наша команда</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/contacts" class="mp-footer__group-link">Контакты</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/ap" class="mp-footer__group-link">Партнерская программа</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/id" class="mp-footer__group-link">Айдентика</a>
+          </p>
+        </div>
+        <div class="mp-footer__group">
+          <p class="mp-footer__group-title">Дизайн</p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/catalog/solution" class="mp-footer__group-link">Выбрать дизайн сайта</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/logotext" class="mp-footer__group-link">Разработка логотипа</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/portfolio" class="mp-footer__group-link">Портфолио</a>
+          </p>
+        </div>
+      </div>
+      <div class="mp-footer__column">
+        <div class="mp-footer__group">
+          <p class="mp-footer__group-title">Тарифы и цены</p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/pages_site" class="mp-footer__group-link">Сайт-Визитка</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/landing-page" class="mp-footer__group-link">Лендинг пейдж</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/base_site" class="mp-footer__group-link">Сайт-Бизнес</a>
+          </p>
+          <p class="mp-footer__group-link-wr _inner">
+            <a href="https://megagroup.ru/base_site/personal" class="mp-footer__group-link">Индивидуальный дизайн</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/base_shop" class="mp-footer__group-link">Интернет-магазин</a>
+          </p>
+          <p class="mp-footer__group-link-wr _inner">
+            <a href="https://megagroup.ru/base_shop/personal" class="mp-footer__group-link">Индивидуальный дизайн</a>
+          </p>
+                  </div>
+        <div class="mp-footer__group">
+          <p class="mp-footer__group-link-wr">
+            <a href="http://help.megagroup.ru/obzor-vozmozhnostey-cms-s3?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Возможности CMS</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/base_shop/features" class="mp-footer__group-link">Возможности магазина</a>
+          </p>
+        </div>
+      </div>
+      <div class="mp-footer__column">
+        <div class="mp-footer__group">
+          <p class="mp-footer__group-title">Наши сервисы</p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://onicon.ru/?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Онлайн-консультант</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://maliver.ru/?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Емайл-рассылки</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://rekmala.ru/?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Контекстная реклама</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="http://xn----8sbg1acauhcdqcjfjcgj.xn--p1ai/?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Конструктор сайтов</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megacrm.ru/?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Управление клиентами</a>
+          </p>
+        </div>
+        <div class="mp-footer__group">
+          <p class="mp-footer__group-title">SEO и продвижение</p>
+          <p class="mp-footer__group-link-wr">
+            <a href="http://seo.megagroup.ru/optimizaciya-sayta?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Оптимизация сайта</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="http://seo.megagroup.ru/prodvizhenie-sayta?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Продвижение сайта</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="http://context.megagroup.ru/?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Контекстное продвижение</a>
+          </p>
+        </div>
+      </div>
+      <div class="mp-footer__column">
+        <div class="mp-footer__group">
+          <p class="mp-footer__group-title">Поддержка</p>
+          <p class="mp-footer__group-link-wr">
+            <a href="http://help.megagroup.ru/?roistat_visit=8881314" target="_blank" rel="nofollow" class="mp-footer__group-link">Руководство пользователя</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/mailing" class="mp-footer__group-link">Обучающая рассылка</a>
+          </p>
+          <div class="mp-footer__socials">
+            <a href="https://vk.com/megagroup_ru?roistat_visit=8881314" rel="nofollow" target="_blank" class="mp-img mp-footer__social">
+              <img src="./Создание и разработка сайтов в Мегагрупп.ру – Москва, Санкт-Петербург и вся Россия_files/ic-vk.svg" alt="">
+            </a>
+            <a href="https://www.youtube.com/megagrouptv?roistat_visit=8881314" rel="nofollow" target="_blank" class="mp-img mp-footer__social">
+              <img src="./Создание и разработка сайтов в Мегагрупп.ру – Москва, Санкт-Петербург и вся Россия_files/ic-youtube.svg" alt="">
+            </a>
+            <a href="https://www.instagram.com/megagroup_ru/?roistat_visit=8881314" rel="nofollow" target="_blank" class="mp-img mp-footer__social">
+              <img src="./Создание и разработка сайтов в Мегагрупп.ру – Москва, Санкт-Петербург и вся Россия_files/ic-insta.svg" alt="">
+            </a>
+            <a href="https://www.facebook.com/megarussia?roistat_visit=8881314" rel="nofollow" target="_blank" class="mp-img mp-footer__social">
+              <img src="./Создание и разработка сайтов в Мегагрупп.ру – Москва, Санкт-Петербург и вся Россия_files/ic-fb.svg" alt="">
+            </a>
+            <a href="https://twitter.com/megagroup?roistat_visit=8881314" rel="nofollow" target="_blank" class="mp-img mp-footer__social">
+              <img src="./Создание и разработка сайтов в Мегагрупп.ру – Москва, Санкт-Петербург и вся Россия_files/ic-twitter.svg" alt="">
+            </a>
+          </div>
+          <div class="mp-footer__callback">
+            <p class="mp-footer__callback-title">Бесплатный звонок</p>
+                        	<a href="tel:88005556342" class="mp-footer__callback-phone" onclick="ga(&#39;send&#39;, &#39;event&#39;, &#39;home_mega_footer&#39;, &#39;phone_free_Russia&#39;, &#39;clik_button&#39;);">8 800 555-63-42</a>
+                      </div>
+          <form class="mp-footer__search js-search" action="https://megagroup.ru/search">
+            <input type="text" name="search" class="cd-input__input mp-footer__search-input " placeholder="Поиск по сайту">
+            <button class="mp-footer__search-sbt">
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill-rule="evenodd" stroke-miterlimit="1.414" clip-rule="evenodd" stroke-linejoin="round"><path fill-rule="nonzero" d="M12.5 20.25a8.75 8.75 0 1 1 8.21-5.717.75.75 0 0 1-1.407-.52 7.25 7.25 0 1 0-1.68 2.617.75.75 0 0 1 1.06 0l3.346 3.34a2.163 2.163 0 1 1-3.059 3.06l-1.5-1.5a.749.749 0 1 1 1.06-1.06l1.5 1.5a.663.663 0 1 0 .939-.938l-2.838-2.834A8.716 8.716 0 0 1 12.5 20.25z"></path></svg>
+            </button>
+          </form>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/msk" class="mp-footer__group-link">Москва</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/spb" class="mp-footer__group-link">Санкт-Петербург</a>
+          </p>
+          <p class="mp-footer__group-link-wr">
+            <a href="https://megagroup.ru/barnaul" class="mp-footer__group-link">Барнаул</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
