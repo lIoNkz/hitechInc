16aba183a5224e1dff531de8a00e2e22422c329d
abik <abik50000@gmail.com>
2018-06-07 13:16:03 +0600

    change contacts

--- resources/views/frontend/contacts.blade.php
+++ resources/views/frontend/contacts.blade.php
@@ -29,55 +29,55 @@
         <h2 class="mp-contacts__block-title">Свяжитесь с нами</h2>
         <ul class="mp-contacts__feedback-list">
                 <li class="mp-contacts__feedback-item">
-            <a href="/mail" class="mp-contacts__feedback-link">
+            <a href="#dddd" class="mp-contacts__feedback-link">
               <img src="/img/oah-mail.svg" class="mp-contacts__feedback-icon" alt="">
               <span class="mp-link">Написать нам</span>
             </a>
           </li>
-                  <li class="mp-contacts__feedback-item">
+<!--                   <li class="mp-contacts__feedback-item">
             <a href="/feedgood" class="mp-contacts__feedback-link">
               <img src="/img/oah-like.svg" class="mp-contacts__feedback-icon" alt="">
               <span class="mp-link">Похвалить</span>
             </a>
           </li>
-                  <li class="mp-contacts__feedback-item">
+<li class="mp-contacts__feedback-item">
             <a href="/feedbad" class="mp-contacts__feedback-link">
               <img src="/img/oah-dislike.svg" class="mp-contacts__feedback-icon" alt="">
               <span class="mp-link">Пожаловаться</span>
             </a>
-          </li>
+          </li> -->
                 </ul>
         <hr class="mp-contacts__line">
         <h2 class="mp-contacts__block-title">Мы в соцсетях</h2>
         <ul class="mp-contacts__feedback-list">
                 <li class="mp-contacts__feedback-item">
-            <a href="https://vk.com/megagroup_ru" rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
+            <a rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
               <img src="/img/oah-social-vk.svg" class="mp-contacts__feedback-icon" alt="">
-              <span class="mp-link">megagroup_ru</span>
+              <span class="mp-link">hitech_hq</span>
             </a>
           </li>
                   <li class="mp-contacts__feedback-item">
-            <a href="https://www.facebook.com/megarussia" rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
+            <a rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
               <img src="/img/oah-social-fb.svg" class="mp-contacts__feedback-icon" alt="">
-              <span class="mp-link">megarussia</span>
+              <span class="mp-link">hitech_hq</span>
             </a>
           </li>
                   <li class="mp-contacts__feedback-item">
-            <a href="https://twitter.com/megagroup"  rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
+            <a rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
               <img src="/img/oah-social-tw.svg" class="mp-contacts__feedback-icon" alt="">
-              <span class="mp-link">@megagroup</span>
+              <span class="mp-link">hitech_hq</span>
             </a>
           </li>
                   <li class="mp-contacts__feedback-item">
-            <a href="https://www.instagram.com/megagroup_ru/"  rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
+            <a   rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
               <img src="/img/oah-social-in.svg" class="mp-contacts__feedback-icon" alt="">
-              <span class="mp-link">megagroup_ru</span>
+              <span class="mp-link">hitech_hq</span>
             </a>
           </li>
                   <li class="mp-contacts__feedback-item">
-            <a href="https://www.youtube.com/megagrouptv" rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
+            <a  rel="nofollow" target="_blank" class="mp-contacts__feedback-link">
               <img src="/img/oah-social-yt.svg" class="mp-contacts__feedback-icon" alt="">
-              <span class="mp-link">megagrouptv</span>
+              <span class="mp-link">hitech_hq</span>
             </a>
           </li>
                 </ul>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <title>InfyOm Generator</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="//cdn.ckeditor.com/4.9.2/standard/ckeditor.js"></script>
+<!--     <script src="//cdn.ckeditor.com/4.9.2/standard/ckeditor.js"></script> -->
     <!-- Bootstrap 3.3.7 -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
