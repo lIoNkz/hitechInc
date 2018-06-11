77239d0142fbf59a08eaed70d692a650ce3e3082
abik <abik50000@gmail.com>
2018-06-11 11:09:50 +0600

    fix feedback 5

--- resources/views/emails/orderFromSite.blade.php
+++ resources/views/emails/orderFromSite.blade.php
@@ -1,7 +1,32 @@
-
+<style>
+	.td {
+		padding: 3px 10px;
+		font-size: 1.1em;
+	}
+</style>
 <h3>Заказ с нашего сайта ( Hitech-hq.kz )</h3>
 <br>
-	<p>	<b>Имя:</b>  {{ $name }}</b> <br></p>
-	<p>	<b>Email:</b>   {{ $email }} <br></p>
+	<p>	  {{ $name }}</b> <br></p>
+	<p>	   {{ $email }} <br></p>
 	<p>	<b>Телефон:</b>   {{ $phone }} <br></p>
 	<p>	<b>Тип услуги:</b>   {{ $service_type }} <br></p>
+<table>
+	<tbody>
+		<tr>
+			<td class="td"><b>Имя:</b></td>
+			<td class="td">{{ $name }}</td>
+		</tr>
+		<tr>
+			<td class="td"><b>Email:</b></td>
+			<td class="td">{{ $email }}</td>
+		</tr>
+		<tr>
+			<td class="td"><b>Телефон:</b></td>
+			<td class="td">{{ $phone }}</td>
+		</tr>
+		<tr>
+			<td class="td"><b>Тип услуги:</b> </td>
+			<td class="td">{{ $service_type }}</td>
+		</tr>
+	</tbody>
+</table>
